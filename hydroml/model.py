9e701ea644bcee079c703143f172b386357f9270
Brandon Boudreaux <hf3209@wayne.edu>
2022-06-30 13:57:55 -0400

    Plotting of layer outputs

--- hydroml/model.py
+++ hydroml/model.py
@@ -6,26 +6,19 @@ class Discriminator(nn.Module):
 
         assert(ndf % 16 == 0 and ndf >= 16)
 
-        self.discrim = nn.Sequential(
-            # Shape: 1 x 128; One channel by 128 elements
-            self._block(1, (ndf // 16), 4, 2, 2, 1),
+        self.ndf = ndf
 
-            #Shape: ndf//16 x 64
-            self._block((ndf // 16), (ndf // 8), 4, 2, 2, 1),
+        self.input_layer = self._input_block()
 
-            # Shape: ndf//8 x 32
-            self._block((ndf // 8), (ndf // 4), 4, 2, 2, 1),
+        self.layer1 = self._block((ndf // 16), (ndf // 8), 4, 2, 2, 1)
 
-            # Shape: ndf//4 x 16
-            self._block((ndf // 4), (ndf // 2), 4, 2, 2, 1),
+        self.layer2 = self._block((ndf // 8), (ndf // 4), 4, 2, 2, 1)
 
-            # Shape: ndf//2 x 8
-            self._block((ndf // 2), (ndf), 4, 2, 2, 1),
+        self.layer3 = self._block((ndf // 4), (ndf // 2), 4, 2, 2, 1)
 
-            # Shape: ndf x 4
-            nn.Conv1d(ndf, 1, 4, 2, 0, 1, bias=False),
-            nn.Sigmoid(),
-        )
+        self.layer4 = self._block((ndf // 2), (ndf), 4, 2, 2, 1)
+
+        self.output_layer = self._output_block()
 
     # Repeatable Layer Block
     def _block(self, channels_in, channels_out, kernel_size, stride, padding, dilation):
@@ -36,16 +29,24 @@ class Discriminator(nn.Module):
             nn.Dropout(p=0.2),
         )
 
-    def forward(self, t):
-        return self.discrim(t)
+    def _input_block(self):
 
-# class Generator(nn.Module):
-#     def __init__(self):
-#         super(Generator, self).__init__()
-#         self.gen = nn.Sequential(
-#             # torch.nn.Conv1d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1,
-#             # bias=True, padding_mode='zeros', device=None, dtype=None)
-#         )
-#
-#     def forward(self, t):
-#         return self.gen(t)
+        return nn.Sequential(
+            # Shape: 1 x 128; One channel by 128 elements
+            self._block(1, (self.ndf // 16), 4, 2, 2, 1),
+        )
+
+    def _output_block(self):
+        # Shape: ndf x 4
+        return nn.Sequential(
+            nn.Conv1d(self.ndf, 1, 4, 2, 0, 1, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, t):
+        t = self.input_layer(t)
+        t = self.layer1(t)
+        t = self.layer2(t)
+        t = self.layer3(t)
+        t = self.layer4(t)
+        return self.output_layer(t)
--- hydroml/plot.py
+++ hydroml/plot.py
@@ -1,5 +1,9 @@
+from collections import OrderedDict
+
 import matplotlib.pyplot as plt
+import torch
 from IPython import display
+import numpy as np
 
 def plot_telemetry(loss_total, real_total, fake_total):
     display.clear_output(wait=True)
@@ -18,6 +22,22 @@ def plot_telemetry(loss_total, real_total, fake_total):
     ax[1].set_title("Mean Output")
     ax[1].set_ylabel("Mean")
     ax[1].set_xlabel("Iterations")
+    ax[1].set_ylim([-0.05, 1.05])
     ax[1].legend()
 
     plt.show()
+
+def layer_hook():
+    def hook(module, input, output):
+        output_array = output.detach().numpy()
+
+        # There are a large number of these, but we will only sample 4.
+        fig = plt.figure(figsize=(20,5))
+        ax = fig.subplots( 4, 4 )
+        for i in range(4):
+            for j in range(4):
+                ax[i][j].plot( np.arange( len(output_array[i][j]) ), output_array[i][j])
+        plt.show()
+
+        return output
+    return hook
